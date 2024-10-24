e411beb40c7bcb4212a34390cf7766527af9873a
Thibault G <thibault.gerardin@gmail.com>
2024-10-24 16:04:27 +0200

    Exercice05

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,18 @@
 def sum(a, b):
+    """
+    Return the sum of a and b
+
+    Take a (int) and b (int)
+    Return a + b (int)
+    """
     return a + b
- 
+
+
 def subtraction(a, b):
+    """
+    Return the substraction of a and b
+
+    Take a (int) and b (int)
+    Return a - b (int)
+    """
     return a - b
