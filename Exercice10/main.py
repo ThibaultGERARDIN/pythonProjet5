d434e1731687f5a751869e628842c4b9c517826d
Thibault G <thibault.gerardin@gmail.com>
2024-10-24 16:25:22 +0200

    Exercice10

--- Exercice10/main.py
+++ Exercice10/main.py
@@ -1,1 +1,24 @@
-## Écrivez votre code ici !
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_details(self):
+        print(f"Nom : {self.name}, Age : {self.age}")
+
+
+class Employee(Person):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+
+    def display_details(self):
+        super().display_details()
+        print(f"Salaire : {self.salary}")
+
+
+john = Person("John", 28)
+employee_alice = Employee("Alice", 30, "30 000€")
+
+john.display_details()
+employee_alice.display_details()
