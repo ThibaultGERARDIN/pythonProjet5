26d605b31b8ff9158d33ced67fd7a6458e3f5fc0
Thibault G <thibault.gerardin@gmail.com>
2024-10-24 15:58:56 +0200

    Exercice03

--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,13 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+
+
+def main():
+    words_length = []
+    for word in words:
+        word_length = (word, len(word))
+        words_length.append(word_length)
+    print(words_length)
+
+
+if __name__ == "__main__":
+    main()
