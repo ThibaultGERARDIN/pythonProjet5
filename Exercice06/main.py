a21ae0d86034b998fc60f92944caafbcec111385
Thibault G <thibault.gerardin@gmail.com>
2024-10-24 16:07:32 +0200

    Exercice06

--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,5 +1,12 @@
 # Fonction calculate_average
- 
+def calculate_average(numbers):
+    numbers_sum = 0
+    for number in numbers:
+        numbers_sum += number
+    average = numbers_sum / len(numbers)
+    return average
+
+
 # Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
 average = calculate_average(numbers)
