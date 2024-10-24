a68d2f3ce3ac485208a167674ea5810b1fd3feb7
Thibault G <thibault.gerardin@gmail.com>
2024-10-24 16:12:55 +0200

    Exercice07

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,15 @@
-## Écrivez votre code ici !
+# Écrivez votre code ici !
+
+
+def square(number):
+    if (type(number) is float) or (type(number) is int):
+        square = number * number
+        return square
+    else:
+        print("Le paramètre doit être un nombre !")
+        return None
+
+
+print(square(2))
+print(square("a"))
+print(square(2.5))
