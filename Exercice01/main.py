3712566346c1b2e6ff60d2a6b20492a63136ed13
Thibault G <thibault.gerardin@gmail.com>
2024-10-24 15:46:11 +0200

    Exercice01

--- Exercice01/main.py
+++ Exercice01/main.py
@@ -1,2 +1,9 @@
 name = input("Entrez votre nom : ")
 age = int(input("Entrez votre âge : "))
+
+
+def main(name, age):
+    print(f"Bonjour, je m'appelle {name} et j'ai {age} ans.")
+
+
+main(name, age)
