51225b1419612421e873c6609214b4812e729351
Thibault G <thibault.gerardin@gmail.com>
2024-10-24 16:15:51 +0200

    Exercice08

--- Exercice08/main.py
+++ Exercice08/main.py
@@ -1,8 +1,15 @@
 def log_decorator(func):
-     pass
- 
+    def wrapper():
+        print("Message avant la fonction")
+        func()
+        print("Message après la fonction")
+
+    return wrapper
+
+
 @log_decorator
 def function_test():
     print("Cette fonction ne prend pas d'arguments.")
+
 
 function_test()
