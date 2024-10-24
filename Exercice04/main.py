eef89491d1a4dc02fd7e71ccaa17ba6ff88f6840
Thibault G <thibault.gerardin@gmail.com>
2024-10-24 16:02:59 +0200

    Exercice04

--- Exercice04/main.py
+++ Exercice04/main.py
@@ -1,14 +1,19 @@
-class myClass:
-def __init__(self, full_name):
-self.full_name=full_name
- 
-def displayName(self):
-print("Le nom complet est :",self.full_name)
+class MyClass:
+    """Print full name"""
 
-class other_class:
-def __init__(self, first_name, name):
-self.first_name=first_name
-self.name=name
- 
-def display_name(self):
-print(f"Nom complet : {self.first_name} {self.name}")
+    def __init__(self, full_name):
+        self.full_name = full_name
+
+    def display_name(self):
+        print(f"Le nom complet est : {self.full_name}")
+
+
+class OtherClass:
+    """Print firstname + name"""
+
+    def __init__(self, first_name, name):
+        self.first_name = first_name
+        self.name = name
+
+    def display_name(self):
+        print(f"Nom complet : {self.first_name} {self.name}")
